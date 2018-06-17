866dd986ceb6d56cf8e6242ac0cf4ac58cafc066
TheEnd023 <sztibi023@gmail.com>
2018-06-17 17:52:21 +0200

    refactor: header design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { RoutingModule } from './routing.module';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     ServiceWorkerModule.register('/ngsw-worker.js', {
