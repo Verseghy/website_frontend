5a74d16758426ee6882fbfa170a349202293a185
TwoDCube <2dcube01@gmail.com>
2018-06-18 09:48:08 +0200

    fix: applied smaller changes to the code to pass CI

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  drawer: boolean;
+  submenu1: boolean;
+  submenu2: boolean;
+
   constructor() { }
 
   ngOnInit() {
