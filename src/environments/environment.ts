8296d835c9efcfae9e58dfb3c301b5e9ed19700b
TwoDCube <2dcube01@gmail.com>
2018-06-20 12:01:12 +0200

    chore: removed firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAhcSKGQGsBx8jEP4zJRI-Hgpbqyma8NXU',
-    authDomain: 'vfghonlap-001.firebaseapp.com',
-    databaseURL: 'https://vfghonlap-001.firebaseio.com',
-    projectId: 'vfghonlap-001',
-    storageBucket: 'vfghonlap-001.appspot.com',
-    messagingSenderId: '442024635461',
-  },
 };
 
 /*
