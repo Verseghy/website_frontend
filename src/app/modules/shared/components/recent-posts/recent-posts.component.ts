872539258dda1e5458e808512acde81517f6ad7d
Zoltán Szepesi <2dcube01@gmail.com>
2018-07-06 23:54:59 +0200

    feature(posts): Add Posts Module (#86)
    * refactor(posts): Move all Post interfaces
    
    * style(posts): lint
    
    * feat: add post module
    
    * style: lint
    
    * style: lint
    
    * refactor: Remove RecentPostsComponent
    
    * feat: add post author section

---
+++ src/app/modules/shared/components/recent-posts/recent-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-posts',
+  templateUrl: './recent-posts.component.html',
+  styleUrls: ['./recent-posts.component.css']
+})
+export class RecentPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
