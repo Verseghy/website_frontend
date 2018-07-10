138b56c3c0124c897b615c6681a681bb04ae00dc
codingoat <codingoat@gmail.com>
2018-07-10 13:19:19 +0200

    feat: added 'subscribe' demo behaviour
    The button looks fancy and validates the email address.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,4 +12,36 @@ export class FooterComponent implements OnInit {
   ngOnInit() {
   }
 
+  subscribeToNewsletter() {
+    const button = document.getElementsByClassName('subscribeButton')[0];
+    const spinner = document.getElementsByClassName('spinner')[0];
+    const buttonText = button.getElementsByClassName('buttonText')[0];
+    const emailInput = <HTMLInputElement>document.getElementsByClassName('emailInput')[0];
+
+
+    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value)) {
+      spinner.removeAttribute('hidden');
+      buttonText.setAttribute('hidden', 'true');
+      button.setAttribute('disabled', 'true');
+
+      console.dir(emailInput.value);
+      emailInput.value = '';
+      setTimeout(() => this.subscribeConfirmed(), 3000);
+    } else {
+      alert('Hibás email-cím!');
+    }
+
+  }
+
+  subscribeConfirmed() {
+    const button = document.getElementsByClassName('subscribeButton')[0];
+    const spinner = document.getElementsByClassName('spinner')[0];
+    const buttonText = button.getElementsByClassName('buttonText')[0];
+
+    spinner.setAttribute('hidden', 'true');
+    buttonText.removeAttribute('hidden');
+    button.className += ' done';
+    buttonText.innerHTML = '✔';
+  }
+
 }
