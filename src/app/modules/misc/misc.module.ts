e9ceb4584953ac36c1644910520153b594f381e5
TwoDCube <2dcube01@gmail.com>
2018-07-29 19:16:34 +0200

    feat: Add misc module and authors component
    Signed-off-by: TwoDCube <2dcube01@gmail.com>

---
+++ src/app/modules/misc/misc.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AuthorsComponent} from './components/authors/authors.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AuthorsComponent]
+})
+export class MiscModule {
+}
