b3ff683983a38329755db035b835ea3aa9a23e65
TwoDCube <2dcube01@gmail.com>
2018-07-29 23:23:13 +0200

    feat: Add design to authors component
    Signed-off-by: TwoDCube <2dcube01@gmail.com>

--- src/app/modules/misc/misc.module.ts
+++ src/app/modules/misc/misc.module.ts
@@ -2,6 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthorsComponent} from './components/authors/authors.component';
 import {RouterModule, Routes} from '@angular/router';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+
+library.add(faGithub, faLinkedin);
 
 const routes: Routes = [
   {
@@ -17,7 +22,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule.forChild(routes)
+    CommonModule,
+    RouterModule.forChild(routes),
+    FontAwesomeModule
   ],
   declarations: [AuthorsComponent]
 })
