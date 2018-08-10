0138d843c396bdd0899e5ed03ea90943da6a0ad9
Zoltán Szepesi <2dcube01@gmail.com>
2018-08-11 00:02:58 +0200

    feat: Added missing pages (#145)
    Signed-off-by: TwoDCube <2dcube01@gmail.com>

--- src/app/modules/misc/misc.module.ts
+++ src/app/modules/misc/misc.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AuthorsComponent } from './components/authors/authors.component';
-import { DocumentsComponent } from './components/documents/documents.component';
-import { RouterModule, Routes } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AuthorsComponent} from './components/authors/authors.component';
+import {DocumentsComponent} from './components/documents/documents.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faFileAlt} from '@fortawesome/free-solid-svg-icons';
+import {ArchiveComponent} from './components/archive/archive.component';
+import {HistoryComponent} from './components/history/history.component';
+import {ClassesComponent} from './components/classes/classes.component';
 
 library.add(faGithub, faLinkedin, faFileAlt);
 
@@ -24,6 +27,18 @@ const routes: Routes = [
     path: 'documents',
     component: DocumentsComponent,
   },
+  {
+    path: 'archive',
+    component: ArchiveComponent,
+  },
+  {
+    path: 'history',
+    component: HistoryComponent,
+  },
+  {
+    path: 'classes',
+    component: ClassesComponent,
+  },
 ];
 
 @NgModule({
@@ -32,7 +47,13 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     FontAwesomeModule
   ],
-  declarations: [AuthorsComponent, DocumentsComponent]
+  declarations: [
+    AuthorsComponent,
+    DocumentsComponent,
+    ArchiveComponent,
+    HistoryComponent,
+    ClassesComponent
+  ]
 })
 export class MiscModule {
 }
