aa6a8df4dbc1f1f73d33d718d576f5317cefe6b2
Zsombor Szabó <codingoat@gmail.com>
2018-08-20 11:11:56 +0200

    feat: Add footer component (#96)
    * refactor: simplify getContrast
    
    Suggested by WebStorm.
    
    close #81
    
    * feat: started working on footer
    
    * feat: adjusted footer design
    
    Changed colors, margins to fit the mobile layout more.
    Renamed footer-container to container.
    
    * feat: added 'subscribe' demo behaviour
    
    The button looks fancy and validates the email address.
    
    * feat: Make content above footer scroll
    
    * feat(home): misc fixes, tickmark has an issue
    
    Issue: fontawesome icon has no native element, thus removing 'hidden' property does not work
    
    * feat: Change footer to use CSS Grid
    
    * fix: Add margin to footer
    
    * fix: Fix merge conflicts
    
    * fix: Change how we import faCheck in footer
    
    * fix: Change fa-icon in footer
    
    * fix: Add ngIf for checkmark
    
    * fix: Spinner uses ngIf
    
    * fix: Email input uses ngModel
    
    * fix: Button Text uses ngIf
    
    * fix: Subscribe button uses ngClass and attribute bindings
    
    * build: Fix package-lock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RoutingModule} from './routing.module';
-import {HeaderComponent} from './components/header/header.component';
-import {HttpClientModule} from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoutingModule } from './routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './components/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
+
+
+library.add(faCheck);
 
 @NgModule({
 
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +32,9 @@ import {HttpClientModule} from '@angular/common/http';
     }),
     BrowserAnimationsModule,
     RoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
