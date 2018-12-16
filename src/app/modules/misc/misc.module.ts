bd83bac2321d048e22b7f35b60961763510f48bc
TwoDCube <2dcube01@gmail.com>
2018-12-16 08:47:17 +0100

    fix: Build error

--- src/app/modules/misc/misc.module.ts
+++ src/app/modules/misc/misc.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AuthorsComponent } from './components/authors/authors.component';
-import { DocumentsComponent } from './components/documents/documents.component';
-import { RouterModule, Routes } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
-import { ArchiveComponent } from './components/archive/archive.component';
-import { HistoryComponent } from './components/history/history.component';
-import { ClassesComponent } from './components/classes/classes.component';
-import { ColleaguesComponent } from './components/colleagues/colleagues.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AuthorsComponent} from './components/authors/authors.component';
+import {DocumentsComponent} from './components/documents/documents.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faFileAlt} from '@fortawesome/free-solid-svg-icons';
+import {ArchiveComponent} from './components/archive/archive.component';
+import {HistoryComponent} from './components/history/history.component';
+import {ClassesComponent} from './components/classes/classes.component';
+import {ColleaguesComponent} from './components/colleagues/colleagues.component';
 
 library.add(faGithub, faLinkedin, faFileAlt);
 
@@ -57,7 +57,8 @@ const routes: Routes = [
     DocumentsComponent,
     ArchiveComponent,
     HistoryComponent,
-    ClassesComponent
+    ClassesComponent,
+    ColleaguesComponent
   ]
 })
 export class MiscModule {
