b143286fc87f6b6479bd2319fd3a38c9c3bf8b36
Zoltán Szepesi <2dcube01@gmail.com>
2019-01-09 18:26:37 +0100

    feat: Add rsync to deploy script (#281)
    * fix: Deploy script
    
    * feat: Add rsync to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -rp -i ./deploy_key ./dist/apps/frontend/* sowpch@verseghy-gimnazium.net:/home/sowpch/beta.verseghy-gimnazium.net/
-
+rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ./deploy_key" ./dist/apps/frontend/ sowpch@verseghy-gimnazium.net:/home/sowpch/beta.verseghy-gimnazium.net/
