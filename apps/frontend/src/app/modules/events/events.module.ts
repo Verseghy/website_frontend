9507e7f590048715218ab004b8585ee075d15c5c
TwoDCube <2dcube01@gmail.com>
2019-01-14 08:47:22 +0100

    feat: Add calendar

--- apps/frontend/src/app/modules/events/events.module.ts
+++ apps/frontend/src/app/modules/events/events.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { EventsComponent } from './components/events/events.component'
 import { RouterModule, Routes } from '@angular/router'
+import { CalendarModule } from '@verseghy/calendar'
 
 const routes: Routes = [
   {
@@ -11,7 +12,11 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    CalendarModule
+  ],
   declarations: [EventsComponent],
 })
 export class EventsModule {}
