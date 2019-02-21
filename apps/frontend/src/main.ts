c2108aec501ad46edf794b80f37e7687aa0587f4
TwoDCube <2dcube01@gmail.com>
2019-02-21 10:50:02 +0100

    QA: Delay angular loading (REVERT THIS COMMIT BEFORE MERGING)

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -11,21 +11,23 @@ if (environment.production) {
   enableProdMode()
 }
 
-const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule)
+const timeout = setTimeout(() => {
+  const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule)
 
-if (environment.hmr) {
-  if (module['hot']) {
-    hmrBootstrap(module, bootstrap)
+  if (environment.hmr) {
+    if (module['hot']) {
+      hmrBootstrap(module, bootstrap)
+    } else {
+      console.error('HMR is not enabled for webpack-dev-server!')
+      console.log('Are you using the --hmr flag for ng serve?')
+    }
   } else {
-    console.error('HMR is not enabled for webpack-dev-server!')
-    console.log('Are you using the --hmr flag for ng serve?')
+    bootstrap()
+      .then(() => {
+        if ('serviceWorker' in navigator && environment.production) {
+          navigator.serviceWorker.register('/ngsw-worker.js')
+        }
+      })
+      .catch(err => console.log(err))
   }
-} else {
-  bootstrap()
-    .then(() => {
-      if ('serviceWorker' in navigator && environment.production) {
-        navigator.serviceWorker.register('/ngsw-worker.js')
-      }
-    })
-    .catch(err => console.log(err))
-}
+}, 5000)
