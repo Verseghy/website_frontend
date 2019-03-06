a016cff868ac801d3efbf498bfcf2a555f1d44de
Zoltán Szepesi <2dcube01@gmail.com>
2019-03-06 23:28:34 +0100

    build: Migrate to Cypress and update packages (#356)
    * build: Migrate to Cypress and update packages
    
    * build: Add Cypress to cache
    
    * build: Fix Cypress not installed
    
    * build: Skip installing cypress at dependencies

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -4,28 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
 
-import 'hammerjs'
-import { hmrBootstrap } from './hmr'
+// import 'hammerjs'
 
 if (environment.production) {
   enableProdMode()
 }
 
-const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule)
-
-if (environment.hmr) {
-  if (module['hot']) {
-    hmrBootstrap(module, bootstrap)
-  } else {
-    console.error('HMR is not enabled for webpack-dev-server!')
-    console.log('Are you using the --hmr flag for ng serve?')
-  }
-} else {
-  bootstrap()
-    .then(() => {
-      if ('serviceWorker' in navigator && environment.production) {
-        navigator.serviceWorker.register('/ngsw-worker.js')
-      }
-    })
-    .catch(err => console.log(err))
-}
+platformBrowserDynamic().bootstrapModule(AppModule)
