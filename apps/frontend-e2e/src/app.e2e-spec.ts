cf3ac1a07b3eb5a84769e012d5d130990a203a00
Zoltan Szepesi <2dcube01@gmail.com>
2019-05-29 01:13:14 +0200

    feat: add frontend protractor e2e

---
+++ apps/frontend-e2e/src/app.e2e-spec.ts
@@ -0,0 +1,12 @@
+import { browser, by, element } from 'protractor'
+
+describe('frontend homepage', () => {
+  it('should load the homepage', async () => {
+    await browser.waitForAngularEnabled(false)
+    await browser.get('http://localhost:4200')
+
+    const verseghytext = element(by.css('h1'))
+
+    await expect(verseghytext.getText()).toEqual('VERSEGHY')
+  })
+})
