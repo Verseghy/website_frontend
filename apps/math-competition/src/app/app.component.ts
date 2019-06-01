6c1955eb84eb8959708b5c89b6cdf24255a35b02
Zoltan Szepesi <2dcube01@gmail.com>
2019-06-02 01:26:15 +0200

    fix: Remove TimeGuard temporarily to allow AppRef to go stable

--- apps/math-competition/src/app/app.component.ts
+++ apps/math-competition/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core'
+import { ApplicationRef, Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'verseghy-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'vfgmath'
+export class AppComponent implements OnInit{
+
+  constructor(private appRef: ApplicationRef) {}
+
+  ngOnInit () {
+    this.appRef.isStable.subscribe((stable) => {console.log(stable)})
+  }
 }
