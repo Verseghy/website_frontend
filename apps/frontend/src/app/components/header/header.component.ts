6cfb982bec47a4be1a7a4e31172cc19223ee9ef5
László Baráth <barath.laci10@gmail.com>
2019-06-03 00:46:03 +0200

    feat: e-Kréta ügyintézés (#523)
    * feat: Add Kreta submenu
    
    * fix: Unnecessary class
    
    * feat: Add e-Kreta in footer
    
    * style: lint
    
    * fix: Requested changes
    
    * style: Format
    
    * fix: Fully override click event handlers
    
    * fix: Footer image sizing
    
    * fix: Center images in list
    
    * fix: Kreta submenu is not responsive
    
    * style: format

--- apps/frontend/src/app/components/header/header.component.ts
+++ apps/frontend/src/app/components/header/header.component.ts
@@ -9,8 +9,14 @@ export class HeaderComponent implements OnInit {
   drawer: boolean
   submenu1: boolean
   submenu2: boolean
-
+  kretaSubmenu: boolean
   constructor() {}
+
+  kretaLink(event: MouseEvent, url: string) {
+    window.open(url, '_blank')
+    this.drawer = !this.drawer
+    event.preventDefault()
+  }
 
   ngOnInit() {}
 }
