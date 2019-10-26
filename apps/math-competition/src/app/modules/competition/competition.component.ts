791fcd1f4672f7baecd3bdb437ada8d33bd65f8d
Zoltan Szepesi <2dcube01@gmail.com>
2019-10-27 01:53:47 +0200

    fix: pagination

--- apps/math-competition/src/app/modules/competition/competition.component.ts
+++ apps/math-competition/src/app/modules/competition/competition.component.ts
@@ -15,10 +15,20 @@ export class CompetitionComponent implements OnInit {
   page = 0
   TEMPdescription = 'Mennyi a következő összeg pontos értéke? $\\left( ctg \\left ( \\frac{\\pi}{4} + \\pi \\right) + tg \\left ( \\frac{\\pi}{4} + \\pi \\right ) )^2 + (ctg \\left ( \\frac{\\pi}{4} + 2\\pi \\right) + tg \\left ( \\frac{\\pi}{4} +2 \\pi \\right ) )^2 + … + (ctg \\left ( \\frac{\\pi}{4} +188 \\pi \\right) + tg \\left ( \\frac{\\pi}{4} + 188 \\pi \\right ) \\right)^2$'
   $TEMParr = new BehaviorSubject<any[]>([])
-  $TEMPPaginated = combineLatest([this.$TEMParr, this.$page]).pipe(
+  $paginated = combineLatest([this.$TEMParr, this.$page]).pipe(
     map(([arr, page]) => {
-      console.log(arr, page)
       return arr.slice(page*10, (page+1)*10)
+    })
+  )
+  $disableNextPage = combineLatest([this.$TEMParr, this.$page]).pipe(
+    map(([arr, page]) => {
+      console.log(arr.length / 10, page)
+      return page + 1 > arr.length / 10
+    })
+  )
+  $disablePrevPage = combineLatest([this.$TEMParr, this.$page]).pipe(
+    map(([arr, page]) => {
+      return page - 1 < 0
     })
   )
 
