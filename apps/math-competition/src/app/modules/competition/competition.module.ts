3c7ad68f488225dd04c8408b8621fc86b3fafdc0
Zoltan Szepesi <2dcube01@gmail.com>
2019-11-24 16:58:36 +0100

    feat: remove mathjax add katex

--- apps/math-competition/src/app/modules/competition/competition.module.ts
+++ apps/math-competition/src/app/modules/competition/competition.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common'
 import { CompetitionRoutingModule } from './competition-routing.module'
 import { CompetitionComponent } from './competition.component'
 import { MdcButtonModule, MdcTextFieldModule, MdcTypographyModule } from '@angular-mdc/web'
-import { PipesModule } from '../../pipes/pipes.module'
+import { KatexModule } from 'ng-katex'
 
 @NgModule({
   declarations: [CompetitionComponent],
-  imports: [CommonModule, CompetitionRoutingModule, MdcTypographyModule, MdcButtonModule, MdcTextFieldModule, PipesModule],
+  imports: [CommonModule, CompetitionRoutingModule, MdcTypographyModule, MdcButtonModule, MdcTextFieldModule, KatexModule],
 })
 export class CompetitionModule {}
