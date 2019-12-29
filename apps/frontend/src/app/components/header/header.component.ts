37e6186417e011aa91cfc44592a1e49224c0aca3
TheEnd023 <sztibi023@gmail.com>
2019-12-29 04:57:12 +0100

    fix: header css

--- apps/frontend/src/app/components/header/header.component.ts
+++ apps/frontend/src/app/components/header/header.component.ts
@@ -3,20 +3,13 @@ import { Component, OnInit } from '@angular/core'
 @Component({
   selector: 'verseghy-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   drawer: boolean
   submenu1: boolean
   submenu2: boolean
-  kretaSubmenu: boolean
   constructor() {}
-
-  kretaLink(event: MouseEvent, url: string) {
-    window.open(url, '_blank')
-    this.drawer = !this.drawer
-    event.preventDefault()
-  }
 
   ngOnInit() {}
 }
