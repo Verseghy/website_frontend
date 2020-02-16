1068dda2a6c08a421b177382ce7f7ea26f766b81
TheEnd023 <sztibi023@gmail.com>
2020-02-16 16:45:59 +0100

    fix: effects

--- apps/frontend/src/app/modules/events/reducer/events.effects.ts
+++ apps/frontend/src/app/modules/events/reducer/events.effects.ts
@@ -1,32 +1,34 @@
 import { Injectable } from '@angular/core'
-import { Actions, Effect, ofType } from '@ngrx/effects'
+import { Actions, createEffect, Effect, ofType } from '@ngrx/effects'
 
 import { Entity, EVENTS_FEATURE_KEY, EventsState } from './events.reducer'
 import { AddLoadedMonth, EventsActionTypes, EventsLoaded, MonthChange } from './events.actions'
-import { Observable, of } from 'rxjs'
-import { Action, select, Store } from '@ngrx/store'
+import { of } from 'rxjs'
+import { select, Store } from '@ngrx/store'
 import { catchError, map, switchMap, withLatestFrom } from 'rxjs/operators'
 import { RequestService } from '../services/request.service'
 
 @Injectable()
 export class EventsEffects {
   @Effect()
-  queryMonth: Observable<Action> | null = this.actions$.pipe(
-    ofType<MonthChange>(EventsActionTypes.MonthChange),
-    withLatestFrom(this.store.pipe(select(EVENTS_FEATURE_KEY))),
-    map(([action, storeState]) => {
-      if (!storeState.loadedMonths.includes(JSON.stringify(action.payload))) {
-        this.store.dispatch(new AddLoadedMonth(JSON.stringify(action.payload)))
-        return this.request.getEvents(action.payload).pipe(catchError(() => of([])))
-      } else {
-        const empty: Entity[] = []
-        return of(empty)
-      }
-    }),
-    switchMap(data => data),
-    switchMap(data => {
-      return of(new EventsLoaded(data))
-    })
+  queryMonth = createEffect(() =>
+    this.actions$.pipe(
+      ofType<MonthChange>(EventsActionTypes.MonthChange),
+      withLatestFrom(this.store.pipe(select(EVENTS_FEATURE_KEY))),
+      map(([action, storeState]) => {
+        if (!storeState.loadedMonths.includes(JSON.stringify(action.payload))) {
+          this.store.dispatch(new AddLoadedMonth(JSON.stringify(action.payload)))
+          return this.request.getEvents(action.payload).pipe(catchError(() => of([])))
+        } else {
+          const empty: Entity[] = []
+          return of(empty)
+        }
+      }),
+      switchMap(data => data),
+      switchMap(data => {
+        return of(new EventsLoaded(data))
+      })
+    )
   )
 
   constructor(private actions$: Actions, private store: Store<EventsState>, private request: RequestService) {}
