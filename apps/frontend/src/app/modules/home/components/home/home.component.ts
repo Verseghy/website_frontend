1bf653d1a79d0e34eac57eef32d90bb758c0840a
TheEnd023 <sztibi023@gmail.com>
2020-02-22 14:53:39 +0100

    feat: recent posts skeleton

--- apps/frontend/src/app/modules/home/components/home/home.component.ts
+++ apps/frontend/src/app/modules/home/components/home/home.component.ts
@@ -4,7 +4,7 @@ import { PostsFacade } from '../../state/posts/posts.facade'
 @Component({
   selector: 'verseghy-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   constructor(private postsFacade: PostsFacade) {}
