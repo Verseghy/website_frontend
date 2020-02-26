ba578f6c9eb5c966b6c485672ed33a0f1d38a09b
TwoDCube <2dcube01@gmail.com>
2020-02-26 01:20:13 +0100

    style: format and lint

--- libs/simple-slideshow/src/lib/slideshow.component.spec.ts
+++ libs/simple-slideshow/src/lib/slideshow.component.spec.ts
@@ -1,25 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { SlideshowComponent } from './slideshow.component';
+import { SlideshowComponent } from './slideshow.component'
 
 describe('SlideshowComponent', () => {
-  let component: SlideshowComponent;
-  let fixture: ComponentFixture<SlideshowComponent>;
+  let component: SlideshowComponent
+  let fixture: ComponentFixture<SlideshowComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SlideshowComponent]
-    })
-      .compileComponents();
-  }));
+      declarations: [SlideshowComponent],
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SlideshowComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(SlideshowComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
--- libs/simple-slideshow/src/lib/slideshow.module.ts
+++ libs/simple-slideshow/src/lib/slideshow.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SlideshowComponent } from './slideshow.component';
-import { BrowserTransferStateModule } from '@angular/platform-browser';
-import { PointerService } from './pointer.service';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { SlideshowComponent } from './slideshow.component'
+import { BrowserTransferStateModule } from '@angular/platform-browser'
+import { PointerService } from './pointer.service'
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserTransferStateModule
-  ],
-  declarations: [
-    SlideshowComponent
-  ],
-  exports: [
-    SlideshowComponent
-  ]
+  imports: [CommonModule, BrowserTransferStateModule],
+  declarations: [SlideshowComponent],
+  exports: [SlideshowComponent],
 })
-export class SlideshowModule { }
+export class SlideshowModule {}
