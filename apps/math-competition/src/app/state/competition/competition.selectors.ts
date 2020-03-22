5b07444a56e12162c57c5a247c7f0110c70f6a99
TwoDCube <2dcube01@gmail.com>
2020-03-22 01:50:37 +0100

    fix: math-competition after @angular-mdc/web update

--- apps/math-competition/src/app/state/competition/competition.selectors.ts
+++ apps/math-competition/src/app/state/competition/competition.selectors.ts
@@ -6,9 +6,13 @@ const selectCompetition = state => state[competitionFeatureKey]
 
 export const selectTeam = createSelector(selectCompetition, state => state.teamID)
 
-export const selectProblems = createSelector(selectCompetition, (competition: State) => competition.problems.sort((a, b) => a.id - b.id))
+export const selectProblems = createSelector(selectCompetition, (competition: State) =>
+  competition.problems.slice().sort((a, b) => a.id - b.id)
+)
 
-export const selectSolutions = createSelector(selectCompetition, (competition: State) => competition.solutions.sort((a, b) => a.id - b.id))
+export const selectSolutions = createSelector(selectCompetition, (competition: State) =>
+  competition.solutions.slice().sort((a, b) => a.id - b.id)
+)
 
 export const selectMerged = createSelector(selectProblems, selectSolutions, (problems, solutions): Merged[] => {
   return problems.map(e => {
