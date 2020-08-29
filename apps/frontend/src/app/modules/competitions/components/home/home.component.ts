797fc5d534d760a8b0e9b86ae353bf2cf92da55d
Szepesi Tibor <sztibi023@gmail.com>
2020-08-29 17:58:44 +0200

    feat: make competitions home page work with the store

--- apps/frontend/src/app/modules/competitions/components/home/home.component.ts
+++ apps/frontend/src/app/modules/competitions/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { CompetitionsFacade } from '../../state/competitions/competitions.facade'
+import { CompetitionRegistration } from '../../models/competition'
 
 @Component({
   selector: 'verseghy-home',
@@ -8,10 +9,17 @@ import { CompetitionsFacade } from '../../state/competitions/competitions.facade
 })
 export class HomeComponent implements OnInit {
 
+  competitions$ = this.competitionsFacade.competitions$
+
   constructor(private competitionsFacade: CompetitionsFacade) { }
 
   ngOnInit(): void {
     this.competitionsFacade.queryRecentCompetitions()
+  }
+
+  isRegistrationOpen(registration: CompetitionRegistration): boolean {
+    const now = new Date().getTime()
+    return now >= registration.start.toMillis() && now <= registration.end.toMillis();
   }
 
 }
