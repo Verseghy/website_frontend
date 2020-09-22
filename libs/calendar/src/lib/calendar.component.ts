53d6129ecf5fc0444a40644d22cd8839e7d1d054
Szepesi Tibor <sztibi023@gmail.com>
2020-09-22 17:41:03 +0200

    feat: new calendar design

--- libs/calendar/src/lib/calendar.component.ts
+++ libs/calendar/src/lib/calendar.component.ts
@@ -25,7 +25,7 @@ import { Renderer } from './lib/renderer'
 @Component({
   selector: 'verseghy-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css'],
+  styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit, AfterViewInit {
   private _cells: Cell[] = []
