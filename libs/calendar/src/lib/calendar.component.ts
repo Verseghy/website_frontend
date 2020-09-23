943ff335b0c30ced2eba23153c7a9ae96f89f459
Szepesi Tibor <sztibi023@gmail.com>
2020-09-23 15:42:39 +0200

    feat: change event background

--- libs/calendar/src/lib/calendar.component.ts
+++ libs/calendar/src/lib/calendar.component.ts
@@ -413,4 +413,39 @@ export class CalendarComponent implements OnInit, AfterViewInit {
       this.moreEventsPopupVisible = false
     }
   }
+
+  getHueFromHEXColor(color: string): number {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color)
+
+    let r = parseInt(result[1], 16)
+    let g = parseInt(result[2], 16)
+    let b = parseInt(result[3], 16)
+    ;(r /= 255), (g /= 255), (b /= 255)
+    const max = Math.max(r, g, b),
+      min = Math.min(r, g, b)
+    let h = (max + min) / 2
+    let s = (max + min) / 2
+    const l = (max + min) / 2
+
+    if (max === min) {
+      h = s = 0
+    } else {
+      const d = max - min
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0)
+          break
+        case g:
+          h = (b - r) / d + 2
+          break
+        case b:
+          h = (r - g) / d + 4
+          break
+      }
+      h /= 6
+    }
+
+    return Math.round(360 * h)
+  }
 }
