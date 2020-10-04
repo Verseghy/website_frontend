ccc95f684bcd1472ea019fe5fbc9313b5175fb7c
Szepesi Tibor <sztibi023@gmail.com>
2020-10-04 22:27:09 +0200

    feat: request service

--- apps/frontend/src/app/modules/pages/components/page/page.component.ts
+++ apps/frontend/src/app/modules/pages/components/page/page.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
-import { map } from 'rxjs/operators'
-import { of } from 'rxjs'
+import { map, switchMap } from 'rxjs/operators'
 import { DomSanitizer, Title } from '@angular/platform-browser'
 import { SubSink } from 'subsink'
+import { RequestService } from '../../services/request.service'
 
 @Component({
   selector: 'verseghy-page',
@@ -15,7 +15,9 @@ export class PageComponent implements OnInit, OnDestroy {
   private subsink = new SubSink()
 
   slug$ = this.route.params.pipe(map(({ slug }) => slug))
-  data$ = of({"id":1,"template":"services","name":"Test Page","title":"Test","slug":"test","content":"<p>seatdfbsdfg<\/p>","extras":{"meta_title":null,"meta_description":null,"meta_keywords":null},"created_at":"2020-10-04T13:21:12.000000Z","updated_at":"2020-10-04T13:21:12.000000Z","deleted_at":null})
+  data$ = this.slug$.pipe(
+    switchMap(slug => this.requestService.getPageBySlug(slug))
+  )
   content$ = this.data$.pipe(
     map(data => {
       return this.domSanitizer.bypassSecurityTrustHtml(data.content)
@@ -24,11 +26,14 @@ export class PageComponent implements OnInit, OnDestroy {
 
   constructor(
     private route: ActivatedRoute,
+    private requestService: RequestService,
     private titleService: Title,
     private domSanitizer: DomSanitizer
   ) { }
 
   ngOnInit(): void {
+
+
     this.subsink.sink = this.data$.subscribe(data => {
       this.titleService.setTitle(data.title)
     })
