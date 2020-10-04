bc949b12960ce1f37b89595c5fd0c862577cdffb
Szepesi Tibor <sztibi023@gmail.com>
2020-10-04 15:24:26 +0200

    feat: create pages module

---
+++ apps/frontend/src/app/modules/pages/pages-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { PageComponent } from './components/page/page.component'
+
+const routes: Routes = [
+  {
+    path: ':slug',
+    component: PageComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
