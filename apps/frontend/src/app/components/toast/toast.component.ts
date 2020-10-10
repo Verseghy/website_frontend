4409c811dd6d4a5f1b9879a2ec6af5819f131630
Szepesi Tibor <sztibi023@gmail.com>
2020-10-10 13:40:42 +0200

    fix: remove debug function

--- apps/frontend/src/app/components/toast/toast.component.ts
+++ apps/frontend/src/app/components/toast/toast.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { ToastService } from '../../services/toast.service'
 
 @Component({
@@ -6,22 +6,8 @@ import { ToastService } from '../../services/toast.service'
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.scss'],
 })
-export class ToastComponent implements OnInit {
+export class ToastComponent {
   toast$ = this.toastService.toast$
 
   constructor(private toastService: ToastService) {}
-
-  ngOnInit(): void {
-    this.toastService.createToast(
-      'This is a random text! This is a random text! This is a random text! This is a random text! This is a random text! This is a random text! This is a random text! This is a random text! This is a random text! ',
-      [
-        {
-          title: 'button 1',
-          callback: () => {
-            console.log('button 1 asd')
-          },
-        },
-      ]
-    )
-  }
 }
