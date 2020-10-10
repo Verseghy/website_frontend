ac2d4b1d89a51074448b66c1c2756b48bc865709
Szepesi Tibor <sztibi023@gmail.com>
2020-10-10 13:38:02 +0200

    feat: working toast

--- apps/frontend/src/app/services/toast.service.ts
+++ apps/frontend/src/app/services/toast.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs'
+import { Toast, ToastAction } from '../models/toast'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
 
+  toast$ = new BehaviorSubject<Toast | null>(null)
+
   constructor() { }
+
+  public createToast(message: string, actions: ToastAction[]): void {
+    this.toast$.next({ message, actions })
+  }
+
+  public removeToast(): void {
+    this.toast$.next(null)
+  }
 }
