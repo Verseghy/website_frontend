49b1204778f3103ea0a0314b0db8dfe9f4490eaf
Szepesi Tibor <sztibi023@gmail.com>
2020-10-10 15:02:58 +0200

    chore: change footer style to scss

--- apps/frontend/src/app/components/footer/footer.component.ts
+++ apps/frontend/src/app/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { environment } from '../../../environments/environment'
 @Component({
   selector: 'verseghy-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
   buttonTextVisible = true
