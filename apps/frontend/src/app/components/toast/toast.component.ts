3724a831d77a8ec5f5ad73d7d780c768009ecced
Szepesi Tibor <sztibi023@gmail.com>
2020-10-10 14:30:02 +0200

    style: format

--- apps/frontend/src/app/components/toast/toast.component.ts
+++ apps/frontend/src/app/components/toast/toast.component.ts
@@ -13,37 +13,45 @@ import { animate, style, transition, trigger } from '@angular/animations'
           opacity: 0,
           transform: 'translate3d(0, calc(100% + 10px), 0)',
         }),
-        animate('200ms ease-out', style({
-          opacity: 1,
-          transform: 'translate3d(0, 0, 0)',
-        }))
+        animate(
+          '200ms ease-out',
+          style({
+            opacity: 1,
+            transform: 'translate3d(0, 0, 0)',
+          })
+        ),
       ]),
       transition(':leave', [
         style({
           opacity: 1,
           transform: 'translate3d(0, 0, 0)',
         }),
-        animate('200ms ease-in', style({
-          opacity: 0,
-          transform: 'translate3d(0, calc(100% + 10px), 0)',
-        }))
-      ])
-    ])
-  ]
+        animate(
+          '200ms ease-in',
+          style({
+            opacity: 0,
+            transform: 'translate3d(0, calc(100% + 10px), 0)',
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
-export class ToastComponent implements OnInit{
+export class ToastComponent implements OnInit {
   toast$ = this.toastService.toast$
 
   constructor(private toastService: ToastService) {}
 
   ngOnInit() {
     setTimeout(() => {
-      this.toastService.createToast('This is a random toast message!', [{
-        title: 'button 1',
-        callback: () => {
-          console.log('Asdasdasdasd')
-        }
-      }])
+      this.toastService.createToast('This is a random toast message!', [
+        {
+          title: 'button 1',
+          callback: () => {
+            console.log('Asdasdasdasd')
+          },
+        },
+      ])
 
       setTimeout(() => {
         this.toastService.removeToast()
