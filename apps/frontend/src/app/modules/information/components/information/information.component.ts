0bf4f21e90bf0f4e891f17ea5e40a987e5ca9764
TwoDCube <2dcube01@gmail.com>
2020-10-13 00:45:06 +0200

    feat: add information component logic

--- apps/frontend/src/app/modules/information/components/information/information.component.ts
+++ apps/frontend/src/app/modules/information/components/information/information.component.ts
@@ -1,35 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { InformationFacade } from '../../state/information/information.facade'
 import { ActivatedRoute } from '@angular/router'
 import { map, switchMap } from 'rxjs/operators'
-import { of } from 'rxjs'
+import { combineLatest, of } from 'rxjs'
 
 @Component({
   selector: 'verseghy-information',
   templateUrl: './information.component.html',
-  styleUrls: ['./information.component.scss']
+  styleUrls: ['./information.component.scss'],
 })
 export class InformationComponent implements OnInit {
-
-  content = 'asdasdasd'
-  title = 'Lorem Ipsum'
-
-  error$ = this.informationFacade.error$
+  error$ = this.informationFacade.error$ // TODO(tiborszepesi): implement errorhandling
   menu$ = this.informationFacade.menu$
+  loadedPage$ = this.informationFacade.loadedPage$ // TODO(tiborszepesi): implement loading
+  loadedMenu$ = this.informationFacade.loadedMenu$ // TODO(tiborszepesi): implement loading
 
-  type$ = this.route.data.pipe(map(({type}) => type))
-  slug$ = this.type$.pipe(
-    switchMap((type) => {
-      if (type === 'slug')
-       return this.route.params.pipe(map(({slug}) => slug))
-      return of(null)
+  slug$ = this.route.params.pipe(map(({ slug }) => slug))
+  page$ = combineLatest([this.informationFacade.page$, this.slug$]).pipe(
+    switchMap(([page, slug]) => {
+      if (!page && slug) {
+        this.informationFacade.queryPageBySlug(slug)
+        return of()
+      }
+
+      return of(page)
     })
   )
 
-  constructor(private route: ActivatedRoute, private informationFacade: InformationFacade) { }
+  constructor(private route: ActivatedRoute, private informationFacade: InformationFacade) {}
 
   ngOnInit(): void {
     this.informationFacade.queryMenu()
   }
-
 }
