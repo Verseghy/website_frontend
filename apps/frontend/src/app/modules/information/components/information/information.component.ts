abcfd24e8ec8058aea66c97e1256c6dada063514
Szepesi Tibor <sztibi023@gmail.com>
2020-10-12 22:53:37 +0200

    WIP

--- apps/frontend/src/app/modules/information/components/information/information.component.ts
+++ apps/frontend/src/app/modules/information/components/information/information.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { InformationFacade } from '../../state/information/information.facade'
+import { ActivatedRoute } from '@angular/router'
+import { map, switchMap } from 'rxjs/operators'
+import { of } from 'rxjs'
 
 @Component({
   selector: 'verseghy-information',
@@ -10,9 +14,22 @@ export class InformationComponent implements OnInit {
   content = 'asdasdasd'
   title = 'Lorem Ipsum'
 
-  constructor() { }
+  error$ = this.informationFacade.error$
+  menu$ = this.informationFacade.menu$
+
+  type$ = this.route.data.pipe(map(({type}) => type))
+  slug$ = this.type$.pipe(
+    switchMap((type) => {
+      if (type === 'slug')
+       return this.route.params.pipe(map(({slug}) => slug))
+      return of(null)
+    })
+  )
+
+  constructor(private route: ActivatedRoute, private informationFacade: InformationFacade) { }
 
   ngOnInit(): void {
+    this.informationFacade.queryMenu()
   }
 
 }
