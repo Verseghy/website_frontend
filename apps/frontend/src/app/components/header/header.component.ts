18e9951d4b86a1fd093baed9a20dc9d2ad23f684
Szepesi Tibor <sztibi023@gmail.com>
2020-10-18 00:39:07 +0200

    style: format

--- apps/frontend/src/app/components/header/header.component.ts
+++ apps/frontend/src/app/components/header/header.component.ts
@@ -25,90 +25,117 @@ const openCloseAnimation = (open: boolean) => {
   styleUrls: ['./header.component.scss'],
   animations: [
     trigger('headerAnimation', [
-      state('open', style({
-        height: '128px',
-      })),
-      state('close', style({
-        height: '64px',
-      })),
+      state(
+        'open',
+        style({
+          height: '128px',
+        })
+      ),
+      state(
+        'close',
+        style({
+          height: '64px',
+        })
+      ),
       transition('close => open', openCloseAnimation(true)),
       transition('open => close', openCloseAnimation(false)),
     ]),
     trigger('headerAnimationWeb', [
-      state('open', style({
-        opacity: 0,
-      })),
-      state('close', style({
-        opacity: 1,
-      })),
-      transition('open <=> close', [
-        animate('300ms')
-      ])
+      state(
+        'open',
+        style({
+          opacity: 0,
+        })
+      ),
+      state(
+        'close',
+        style({
+          opacity: 1,
+        })
+      ),
+      transition('open <=> close', [animate('300ms')]),
     ]),
     trigger('headerAnimationTitleEnd', [
-      state('open', style({
-        opacity: 1,
-      })),
-      state('close', style({
-        opacity: 0,
-      })),
-      transition('open <=> close', [
-        animate('300ms')
-      ])
+      state(
+        'open',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'close',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('open <=> close', [animate('300ms')]),
     ]),
     trigger('headerAnimationRight', [
-      state('open', style({
-        transform: 'scale3d(1.4, 1.4, 1.4)',
-      })),
-      state('close', style({
-        transform: 'scale3d(1, 1, 1)',
-      })),
-      transition('open <=> close', [
-        animate('300ms')
-      ])
+      state(
+        'open',
+        style({
+          transform: 'scale3d(1.4, 1.4, 1.4)',
+        })
+      ),
+      state(
+        'close',
+        style({
+          transform: 'scale3d(1, 1, 1)',
+        })
+      ),
+      transition('open <=> close', [animate('300ms')]),
     ]),
     trigger('headerAnimationTitle', [
-      state('open', style({
-        letterSpacing: '1px',
-      })),
-      state('close', style({
-        letterSpacing: '0px',
-      })),
-      transition('open <=> close', [
-        animate('300ms')
-      ])
+      state(
+        'open',
+        style({
+          letterSpacing: '1px',
+        })
+      ),
+      state(
+        'close',
+        style({
+          letterSpacing: '0px',
+        })
+      ),
+      transition('open <=> close', [animate('300ms')]),
     ]),
     trigger('headerAnimationImage', [
-      state('open', style({
-        height: '75px',
-      })),
-      state('close', style({
-        height: '48px',
-      })),
-      transition('open <=> close', [
-        animate('300ms ease-in-out')
-      ])
+      state(
+        'open',
+        style({
+          height: '75px',
+        })
+      ),
+      state(
+        'close',
+        style({
+          height: '48px',
+        })
+      ),
+      transition('open <=> close', [animate('300ms ease-in-out')]),
     ]),
     trigger('headerAnimationLogoBottom', [
-      state('open', style({
-        opacity: '1',
-        letterSpacing: '0',
-      })),
-      state('close', style({
-        opacity: '0',
-        letterSpacing: '-5px',
-      })),
-      transition('open => close', [
-        animate('150ms ease-in-out')
-      ]),
-      transition('close => open', [
-        animate('150ms ease-in-out')
-      ])
-    ])
-  ]
+      state(
+        'open',
+        style({
+          opacity: '1',
+          letterSpacing: '0',
+        })
+      ),
+      state(
+        'close',
+        style({
+          opacity: '0',
+          letterSpacing: '-5px',
+        })
+      ),
+      transition('open => close', [animate('150ms ease-in-out')]),
+      transition('close => open', [animate('150ms ease-in-out')]),
+    ]),
+  ],
 })
 export class HeaderComponent {
-
   scrollEvent$ = fromEvent(document, 'scroll', { passive: true }).pipe(startWith(0))
   resizeEvent$ = fromEvent(window, 'resize', { passive: true }).pipe(startWith(0))
   openHeader$ = combineLatest([this.headerService.useBigHeader$, this.scrollEvent$, this.resizeEvent$]).pipe(
@@ -119,7 +146,6 @@ export class HeaderComponent {
       return document.documentElement.scrollTop < 64 ? 'open' : 'close'
     })
   )
-
 
   drawer: boolean
   submenu1: boolean
