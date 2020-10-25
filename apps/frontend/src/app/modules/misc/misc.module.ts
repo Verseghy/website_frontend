35980017c9d73f2736e9bb61353812d1570cfafd
Szepesi Tibor <sztibi023@gmail.com>
2020-10-25 16:05:04 +0100

    feat: remove unnecessary misc pages

--- apps/frontend/src/app/modules/misc/misc.module.ts
+++ apps/frontend/src/app/modules/misc/misc.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { AuthorsComponent } from './components/authors/authors.component'
-import { DocumentsComponent } from './components/documents/documents.component'
 import { RouterModule, Routes } from '@angular/router'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faFileAlt } from '@fortawesome/free-solid-svg-icons'
-import { HistoryComponent } from './components/history/history.component'
-import { ClassesComponent } from './components/classes/classes.component'
 import { ColleaguesComponent } from './components/colleagues/colleagues.component'
 import { StoreModule } from '@ngrx/store'
 import { EffectsModule } from '@ngrx/effects'
@@ -29,18 +26,6 @@ const routes: Routes = [
     component: AuthorsComponent,
   },
   {
-    path: 'documents',
-    component: DocumentsComponent,
-  },
-  {
-    path: 'history',
-    component: HistoryComponent,
-  },
-  {
-    path: 'classes',
-    component: ClassesComponent,
-  },
-  {
     path: 'colleagues',
     component: ColleaguesComponent,
   },
@@ -55,6 +40,6 @@ const routes: Routes = [
     StoreModule.forFeature(COLLEAGUES_FEATURE_KEY, colleaguesReducer, { initialState: colleaguesInitialState }),
     EffectsModule.forFeature([ColleaguesEffects]),
   ],
-  declarations: [AuthorsComponent, DocumentsComponent, HistoryComponent, ClassesComponent, ColleaguesComponent],
+  declarations: [AuthorsComponent, ColleaguesComponent],
 })
 export class MiscModule {}
