5f020f53d72bba1b97b52532be5233e6e301dfe1
TwoDCube <2dcube01@gmail.com>
2020-11-19 17:37:40 +0100

    feat: Implement server state transfer, fix some visual bugs

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser'
+import { BrowserModule, BrowserTransferStateModule, makeStateKey, TransferState } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
@@ -10,7 +10,7 @@ import { FooterComponent } from './components/footer/footer.component'
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { FormsModule } from '@angular/forms'
 import { NxModule } from '@nrwl/angular'
-import { StoreModule } from '@ngrx/store'
+import { ActionReducer, Store, StoreModule } from '@ngrx/store'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { EffectsModule } from '@ngrx/effects'
 import { AngularFireModule } from '@angular/fire'
@@ -23,9 +23,21 @@ import { ToastComponent } from './components/toast/toast.component'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
 
+const stateSetter = (reducer: ActionReducer<any>): ActionReducer<any> => {
+  return function (state: any, action: any) {
+    if (action.type === 'SET_ROOT_STATE') {
+      return action.payload
+    }
+    return reducer(state, action)
+  }
+}
+
+export const NGRX_STATE = makeStateKey('NGRX_STATE')
+
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, ToastComponent, PageNotFoundComponent],
   imports: [
+    BrowserTransferStateModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     RoutingModule,
@@ -34,7 +46,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     FormsModule,
     LoadersModule,
     NxModule.forRoot(),
-    StoreModule.forRoot({}),
+    StoreModule.forRoot({}, { metaReducers: [stateSetter] }),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
@@ -53,7 +65,33 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor(library: FaIconLibrary) {
+  constructor(library: FaIconLibrary, private transferState: TransferState, private store: Store<any>) {
     library.addIcons(faFacebookF)
+
+    const isBrowser = this.transferState.hasKey<any>(NGRX_STATE)
+
+    if (isBrowser) {
+      this.onBrowser()
+    } else {
+      this.onServer()
+    }
+  }
+
+  onServer() {
+    this.transferState.onSerialize(NGRX_STATE, () => {
+      let state
+      this.store
+        .subscribe((saveState: any) => {
+          state = saveState
+        })
+        .unsubscribe()
+      return state
+    })
+  }
+
+  onBrowser() {
+    const state = this.transferState.get<any>(NGRX_STATE, null)
+    this.transferState.remove(NGRX_STATE)
+    this.store.dispatch({ type: 'SET_ROOT_STATE', payload: state })
   }
 }
