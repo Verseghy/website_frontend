f32071727f7de1c4b4d3b94b3efa9d7d1425d424
TwoDCube <2dcube01@gmail.com>
2020-11-22 02:25:00 +0100

    feat: add admin claim and guards

---
+++ apps/math-competition/src/app/guards/if-admin-redirect.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core'
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router'
+import { Observable } from 'rxjs'
+import { AngularFireAuth } from '@angular/fire/auth'
+import { map } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IfAdminRedirectGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.afa.idTokenResult.pipe(
+      map((token) => {
+        if (!token) return true
+        if (token.claims.admin) {
+          return this.router.createUrlTree(['/admin'])
+        }
+
+        return true
+      })
+    )
+  }
+
+  constructor(private afa: AngularFireAuth, private router: Router) {}
+}
