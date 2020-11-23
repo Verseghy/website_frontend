55f8526c0856e47c840e19af1cf6ff4da6479dc8
Szepesi Tibor <sztibi023@gmail.com>
2020-11-23 12:14:20 +0100

    feat: add admin page design

--- apps/math-competition/src/app/modules/admin/admin.module.ts
+++ apps/math-competition/src/app/modules/admin/admin.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { AdminRoutingModule } from './admin-routing.module'
-import { EditComponent } from './components/edit/edit.component'
 import { ListComponent } from './components/list/list.component'
+import { KatexModule } from 'ng-katex'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatSelectModule } from '@angular/material/select'
 
 @NgModule({
-  declarations: [ListComponent, EditComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  declarations: [ListComponent],
+  imports: [CommonModule, AdminRoutingModule, MatSelectModule, MatPaginatorModule, KatexModule],
 })
 export class AdminModule {}
--- apps/math-competition/src/app/modules/admin/components/list/list.component.ts
+++ apps/math-competition/src/app/modules/admin/components/list/list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core'
+import { AuthFacade } from '../../../../state/auth/auth.facade'
+import { CompetitionFacade } from '../../../../state/competition/competition.facade'
+import { PageEvent } from '@angular/material/paginator'
+import { BehaviorSubject, combineLatest } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { Problem } from '../../../../interfaces/problem.interface'
 
 @Component({
   selector: 'verseghy-list',
@@ -6,7 +12,47 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-  constructor() {}
+  pageProperties$ = new BehaviorSubject<PageEvent>({ pageIndex: 0, pageSize: 10, length: 0 })
+  length$ = this.competitionFacade.problems$.pipe(map((problems) => problems.length))
+  problems$ = combineLatest([this.competitionFacade.problems$, this.pageProperties$]).pipe(
+    map(([problems, properties]) => {
+      const start = properties.pageIndex * properties.pageSize
+      const end = start + properties.pageSize
+      return problems.slice(start, end)
+    })
+  )
 
-  ngOnInit(): void {}
+  constructor(private authFacade: AuthFacade, private competitionFacade: CompetitionFacade) {}
+
+  ngOnInit(): void {
+    this.competitionFacade.loadProblems()
+  }
+
+  logout() {
+    this.authFacade.logout()
+  }
+
+  switchPage(event: PageEvent) {
+    this.pageProperties$.next(event)
+  }
+
+  trackByFn(index: number, problem: Problem): number {
+    return problem.id
+  }
+
+  input(event: Event, problem: Problem): void {
+    const newProblem = Object.assign({}, problem)
+    const target = event.target as HTMLTextAreaElement
+    newProblem.text = target.value
+
+    // TODO: remove this
+    // @ts-ignore
+    delete newProblem.type
+
+    this.competitionFacade.setProblem(problem)
+  }
+
+  newProblem(): void {
+    console.log('new problem')
+  }
 }
