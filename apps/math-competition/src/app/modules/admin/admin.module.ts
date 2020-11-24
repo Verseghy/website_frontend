5f5074ed6aa6f14eb009506abf71d6cfd2ef350f
Szepesi Tibor <sztibi023@gmail.com>
2020-11-24 11:52:54 +0100

    feat: add problem remove button

--- apps/math-competition/src/app/modules/admin/admin.module.ts
+++ apps/math-competition/src/app/modules/admin/admin.module.ts
@@ -6,9 +6,10 @@ import { ListComponent } from './components/list/list.component'
 import { KatexModule } from 'ng-katex'
 import { MatPaginatorModule } from '@angular/material/paginator'
 import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [ListComponent],
-  imports: [CommonModule, AdminRoutingModule, MatSelectModule, MatPaginatorModule, KatexModule],
+  imports: [CommonModule, AdminRoutingModule, MatSelectModule, MatPaginatorModule, KatexModule, MatButtonModule],
 })
 export class AdminModule {}
