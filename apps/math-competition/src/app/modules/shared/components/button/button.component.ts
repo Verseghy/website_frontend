7426cbe1b041e38b0de29b32da1b456ddc786940
Szepesi Tibor <sztibi023@gmail.com>
2020-12-24 16:19:47 +0100

    feat: outlined button

--- apps/math-competition/src/app/modules/shared/components/button/button.component.ts
+++ apps/math-competition/src/app/modules/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 
 @Component({
   selector: 'verseghy-button',
@@ -6,5 +6,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
+  @Input() outlined = false
+
   constructor() {}
 }
