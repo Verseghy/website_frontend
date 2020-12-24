ee57b22ee33b195ef58b0498db9f1a061f4e964d
Szepesi Tibor <sztibi023@gmail.com>
2020-12-24 16:43:28 +0100

    feat: input type password

--- apps/math-competition/src/app/modules/shared/components/input/input.component.ts
+++ apps/math-competition/src/app/modules/shared/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 
 @Component({
   selector: 'verseghy-input',
@@ -6,5 +6,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InputComponent {
+  @Input() type = 'text'
   constructor() {}
 }
