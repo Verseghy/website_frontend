5271654898e30a1354898ee717c7e0eb5b24268d
Zoltán Szepesi <2dcube01@gmail.com>
2020-12-25 16:56:11 +0100

    style: format

--- apps/math-competition/src/app/modules/shared/components/input/input.component.ts
+++ apps/math-competition/src/app/modules/shared/components/input/input.component.ts
@@ -11,8 +11,8 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms'
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => InputComponent),
       multi: true,
-    }
-  ]
+    },
+  ],
 })
 export class InputComponent implements ControlValueAccessor {
   @Input() type = 'text'
