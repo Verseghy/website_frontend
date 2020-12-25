c6916ee6f16d2afcb94d097f1b2203452de7bb8d
Szepesi Tibor <sztibi023@gmail.com>
2020-12-25 15:42:59 +0100

    feat: login page error handling

--- apps/math-competition/src/app/modules/shared/components/button/button.component.ts
+++ apps/math-competition/src/app/modules/shared/components/button/button.component.ts
@@ -7,6 +7,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 })
 export class ButtonComponent {
   @Input() outlined = false
+  @Input() disabled = false
 
   constructor() {}
 }
