31bc63cb9ed0d8ffe3f3677e1903319d6d89bf26
Zoltán Szepesi <2dcube01@gmail.com>
2020-12-25 16:46:32 +0100

    chore: remove unused imports

--- apps/math-competition/src/app/modules/competition/competition.module.ts
+++ apps/math-competition/src/app/modules/competition/competition.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common'
 
 import { CompetitionRoutingModule } from './competition-routing.module'
 import { CompetitionComponent } from './competition.component'
-import { MdcButtonModule, MdcTextFieldModule, MdcTypographyModule } from '@angular-mdc/web'
 import { KatexModule } from 'ng-katex'
 import { TimerComponent } from './components/timer/timer.component'
 import { AngularFireStorageModule } from '@angular/fire/storage'
@@ -13,9 +12,6 @@ import { AngularFireStorageModule } from '@angular/fire/storage'
   imports: [
     CommonModule,
     CompetitionRoutingModule,
-    MdcTypographyModule,
-    MdcButtonModule,
-    MdcTextFieldModule,
     KatexModule,
     AngularFireStorageModule,
   ],
