8a68d621a35f3bb7702621716fee1972fe0d044e
Zoltán Szepesi <2dcube01@gmail.com>
2020-12-25 17:30:12 +0100

    build: update Angular to v11

--- libs/ui/src/lib/loaders/loaders.module.spec.ts
+++ libs/ui/src/lib/loaders/loaders.module.spec.ts
@@ -1,8 +1,8 @@
-import { async, TestBed } from '@angular/core/testing'
+import { TestBed, waitForAsync } from '@angular/core/testing'
 import { LoadersModule } from './loaders.module'
 
 describe('LoadersModule', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [LoadersModule],
     }).compileComponents()
