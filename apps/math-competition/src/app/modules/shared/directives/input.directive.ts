24120d2904b663c684423eb5ad63a71b8558edd1
Szepesi Tibor <sztibi023@gmail.com>
2021-01-30 15:34:06 +0100

    feat: change competition inputs to type numeric

--- apps/math-competition/src/app/modules/shared/directives/input.directive.ts
+++ apps/math-competition/src/app/modules/shared/directives/input.directive.ts
@@ -4,7 +4,7 @@ import { Directive } from '@angular/core'
   selector: 'input[verseghyInput]',
   // tslint:disable-next-line:no-host-metadata-property
   host: {
-    class: 'px-4 py-2 border focus:ring-1 ring-primary-600 dark:ring-primary-400 border-gray-500 focus:outline-none focus:border-primary-600 dark:focus:border-primary-400 rounded-md bg-transparent w-full',
+    class: 'px-4 py-2 border focus:ring-1 ring-primary-600 border-gray-500 focus:outline-none focus:border-primary-600 dark:focus:border-primary-400 rounded-md bg-transparent w-full',
   }
 })
 export class InputDirective {
