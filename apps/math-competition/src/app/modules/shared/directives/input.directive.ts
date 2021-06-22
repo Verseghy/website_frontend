70b1fdcbb8ccfc7847372a0fd687c50630669e19
TwoDCube <2dcube01@gmail.com>
2021-06-22 23:11:17 +0200

    fix: run eslint migration

--- apps/math-competition/src/app/modules/shared/directives/input.directive.ts
+++ apps/math-competition/src/app/modules/shared/directives/input.directive.ts
@@ -2,7 +2,7 @@ import { Directive } from '@angular/core'
 
 @Directive({
   selector: '[verseghyInput]',
-  // tslint:disable-next-line:no-host-metadata-property
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
   host: {
     class:
       'px-4 py-2 border focus:ring-1 ring-primary-600 border-gray-500 focus:outline-none focus:border-primary-600 dark:focus:border-primary-400 rounded-md bg-transparent w-full',
