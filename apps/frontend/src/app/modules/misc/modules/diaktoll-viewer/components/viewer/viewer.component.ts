d95c3d76476a63a30902803425b15b30c2a0621e
Brigitta Csajka <csajkusz@gmail.com>
2021-06-26 16:57:39 +0200

    viewer

---
+++ apps/frontend/src/app/modules/misc/modules/diaktoll-viewer/components/viewer/viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'verseghy-viewer',
+  templateUrl: './viewer.component.html',
+  styleUrls: ['./viewer.component.css']
+})
+export class ViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
