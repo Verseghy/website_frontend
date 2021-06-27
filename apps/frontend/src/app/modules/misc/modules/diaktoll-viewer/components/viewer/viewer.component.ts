ed233737c611c0daf16768f5d3618e3370d4be70
Brigitta Csajka <csajkusz@gmail.com>
2021-06-27 14:35:18 +0200

    fix(diaktoll): properly handling resize events

--- apps/frontend/src/app/modules/misc/modules/diaktoll-viewer/components/viewer/viewer.component.ts
+++ apps/frontend/src/app/modules/misc/modules/diaktoll-viewer/components/viewer/viewer.component.ts
@@ -1,13 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { map, takeUntil } from 'rxjs/operators'
+import { fromEvent, Observable } from 'rxjs'
+
+const breakpoints = {
+  Medium: '(max-width: 991.99px)',
+  Large: '(min-width: 992px)'
+}
 
 @Component({
   selector: 'verseghy-viewer',
   templateUrl: './viewer.component.html',
-  styleUrls: ['./viewer.component.css']
+  styleUrls: ['./viewer.component.scss']
 })
 export class ViewerComponent implements OnInit {
+  @HostBinding('class.no-absolute') hasClass = false;
+  zoom$: Observable<number> = this.breakpointObserver.observe([
+    breakpoints.Medium,
+    breakpoints.Large
+  ]).pipe(
+    map(res => {
+      console.table(res.breakpoints);
+      if(res.breakpoints[breakpoints.Medium]) return 1;
+      if(res.breakpoints[breakpoints.Large]) return 0.5;
+    })
+  );
 
-  constructor() { }
+  width$ = fromEvent(window, 'resize').pipe(map(e => window.innerWidth > 992 ? '992px' : `${window.innerWidth}px`))
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
 
   ngOnInit(): void {
   }
