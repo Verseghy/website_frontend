985ca0417ebea9ad13ef01efa5bfa442669754ef
Szepesi Tibor <sztibi023@gmail.com>
2021-08-28 18:23:59 +0200

    feat: information page keyboard navigation

--- apps/frontend/src/app/modules/information/components/sidebar/sidebar.component.ts
+++ apps/frontend/src/app/modules/information/components/sidebar/sidebar.component.ts
@@ -1,50 +1,22 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 import { MenuItem } from '../../models/menu-item'
-import { Router } from '@angular/router'
-import { InformationFacade } from '../../state/information/information.facade'
-import { animate, state, style, transition, trigger } from '@angular/animations'
 
 @Component({
   selector: 'verseghy-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  animations: [
-    trigger('dropdown', [
-      state(
-        'open',
-        style({
-          height: '100%',
-        })
-      ),
-      state(
-        'closed',
-        style({
-          height: '0',
-        })
-      ),
-      transition('open <=> close', [
-        animate(
-          '300ms',
-          style({
-            height: '*',
-          })
-        ),
-      ]),
-    ]),
-  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
   @Input() data: MenuItem[]
 
   dropdownOpen = false
 
-  constructor(private router: Router, private informationFacade: InformationFacade) {}
+  constructor() {}
 
-  ngOnInit(): void {}
+  keydown(event: KeyboardEvent) {
+    if (event.code !== 'Enter') return
 
-  loadPage(slug: string): void {
-    this.informationFacade.queryPageBySlug(slug)
-    this.router.navigate(['/information', slug])
+    this.dropdownOpen = !this.dropdownOpen
   }
 }
