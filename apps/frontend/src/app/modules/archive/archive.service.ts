9dd0ac99e3ffe147155057e18f63b893edae4fbb
TwoDCube <2dcube01@gmail.com>
2022-01-21 00:36:54 +0100

    feat: add archive gql

--- apps/frontend/src/app/modules/archive/archive.service.ts
+++ apps/frontend/src/app/modules/archive/archive.service.ts
@@ -1,23 +1,67 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { environment } from '../../../environments/environment'
+import {Apollo, gql} from "apollo-angular";
+import {map} from "rxjs/operators";
+import {Observable} from "rxjs";
+import {Post} from "../../models/Post";
+
+const infoQUERY = gql`
+  query {
+    archive {
+      info {
+        count
+        year
+        month
+      }
+    }
+  }
+`
+
+const postsQUERY = gql`
+  query Archive($year: Int!, $month: Int!) {
+    archive {
+      posts(year: $year, month: $month)
+    }
+  }
+`
+
+interface Result {
+  archive: {
+    info: Stats[]
+    posts: Post[]
+  }
+}
+
+export interface Stats { count: number, year: number, month: number}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArchiveService {
-  getArchives() {
-    return this.http.get(environment.baseURL + '/posts/getCountByMonth')
+  constructor(private gql: Apollo) {}
+
+  getArchives(): Observable<Stats[]> {
+    return this.gql.watchQuery<Result>({
+      query: infoQUERY
+    }).valueChanges.pipe(
+      map(res => {
+        return res.data.archive.info
+      })
+    )
   }
 
-  getDetailedArchives({ year, month }: { year: number; month: number }) {
-    return this.http.get(environment.baseURL + '/posts/getPostsByYearMonth', {
-      params: {
-        year: year.toString(10),
-        month: month.toString(10),
-      },
-    })
+  getDetailedArchives({ year, month }: { year: number; month: number }): Observable<Post[]> {
+    return this.gql.watchQuery<Result>({
+      query: infoQUERY,
+      variables: {
+        year,
+        month,
+      }
+    }).valueChanges.pipe(
+      map(res => {
+        return res.data.archive.posts
+      })
+    )
   }
-
-  constructor(private http: HttpClient) {}
 }
