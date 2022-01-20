a959a04b19aed62197fe2c78fcd417c61a512ea4
TwoDCube <2dcube01@gmail.com>
2022-01-21 00:47:13 +0100

    feat: add menu gql

--- apps/frontend/src/app/modules/information/service/request.service.ts
+++ apps/frontend/src/app/modules/information/service/request.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { environment } from '../../../../environments/environment.prod'
 import { Observable } from 'rxjs'
 import { PageData } from '../../../models/page'
 import { MenuItem } from '../models/menu-item'
