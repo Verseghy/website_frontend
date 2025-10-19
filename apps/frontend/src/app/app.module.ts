7f9726bb1cd57732bb5fa4b4b891072c65542395
Szepesi Tibor <smrtrfszm@proton.me>
2025-10-19 19:54:27 +0200

    Format

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -20,11 +20,11 @@ import { ToastComponent } from './components/toast/toast.component'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
 import { LayoutModule } from '@angular/cdk/layout'
-import {APOLLO_OPTIONS} from "apollo-angular";
-import {HttpLink} from "apollo-angular/http";
-import {InMemoryCache} from "@apollo/client/core";
-import {createPersistedQueryLink} from "apollo-angular/persisted-queries";
-import {sha256} from "crypto-hash";
+import { APOLLO_OPTIONS } from 'apollo-angular'
+import { HttpLink } from 'apollo-angular/http'
+import { InMemoryCache } from '@apollo/client/core'
+import { createPersistedQueryLink } from 'apollo-angular/persisted-queries'
+import { sha256 } from 'crypto-hash'
 
 const stateSetter = (reducer: ActionReducer<any>): ActionReducer<any> => {
   return function (state: any, action: any) {
@@ -61,17 +61,15 @@ export const NGRX_STATE = makeStateKey('NGRX_STATE')
       provide: APOLLO_OPTIONS,
       useFactory: (httpLink: HttpLink) => {
         const link = createPersistedQueryLink({
-          sha256
-        }).concat(
-          httpLink.create({uri: environment.gqlURL}),
-        );
+          sha256,
+        }).concat(httpLink.create({ uri: environment.gqlURL }))
         return {
           cache: new InMemoryCache(),
-          link
+          link,
         }
       },
-      deps: [HttpLink]
-    }
+      deps: [HttpLink],
+    },
   ],
   bootstrap: [AppComponent],
 })
